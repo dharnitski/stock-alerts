0c378374941f68189dfa56e1f2f47bac1004920d
Dmitry Harnitski <dmitry.harnitski@careerbuilder.com>
2018-03-22 22:17:38 -0400

    test handler - 100% coverage

--- src/parser/test/upload/handler.spec.js
+++ src/parser/test/upload/handler.spec.js
@@ -1,12 +1,27 @@
 'use strict';
 /* global describe it beforeEach */
 
+var expect = require('chai').expect;
+
+const myLambda = require('../../upload/handler');
+
 describe('Upload', () => {
 
     describe('handler', () => {
 
-        it('should return 200', () => {
-            
+        it('should return 200', (done) => {
+
+            myLambda.handler({}, { /* context */ }, (err, result) => {
+                try {
+                    expect(err).to.not.exist;
+                    expect(result).to.exist;
+                    expect(result.statusCode).to.equal(200);
+                    done();
+                }
+                catch (error) {
+                    done(error);
+                }
+            });
         });
     });
 });
