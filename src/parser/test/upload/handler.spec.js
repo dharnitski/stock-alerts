c31b192ba05a7191796fb8429315134c0a3d56f7
Dmitry Harnitski <dmitry.harnitski@careerbuilder.com>
2018-03-22 22:01:12 -0400

    add jest

---
+++ src/parser/test/upload/handler.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+/* global describe it beforeEach */
+
+describe('Upload', () => {
+
+    describe('handler', () => {
+
+        it('should return 200', () => {
+            
+        });
+    });
+});
