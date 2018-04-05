d760b17c5efc0703295e2f705a8c4a9ea6f87d9d
Dmitry Harnitski <dharnitski@gmail.com>
2018-04-04 23:58:13 -0400

    load list of halts in frontend

--- parser/results/handler.js
+++ parser/results/handler.js
@@ -7,6 +7,10 @@ module.exports.handler = (event, context, callback) => {
         result => {
             const response = {
                 statusCode: 200,
+                headers: {
+                    "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+                    "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+                },
                 body: JSON.stringify(result.Items),
             };
             callback(null, response);
